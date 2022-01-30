1e708ac87e23f4606019ff4e1e8f88cdad359e48
Julien MIQUEL <julienmiquel@google.com>
2022-01-30 10:51:44 +0100

    Define state on terraform gcs

--- settings.tf
+++ settings.tf
@@ -3,9 +3,15 @@
 data "terraform_remote_state" "remote_tf_states" {
   backend = "gcs"
   config = {
-    bucket  = var.gcs_tf_state
+    bucket  = "tf_state_devops_hackathon_cagip"
     prefix  = format("env_%s", var.tag)
   }
 }
 
 
+terraform {
+  backend "gcs" {
+    bucket  = "tf_state_devops_hackathon_cagip"
+    prefix  = "terraform/state"
+  }
+}
